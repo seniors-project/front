87c7f58aa5ae7a273caec347e4257be2fa76827b
ggujin <yang97816@gmail.com>
2023-07-10 23:52:15 +0900

    style: add Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import Head from 'next/head';
 
+import { Layout } from '@/component/Layout';
+
 function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>setting-practice</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Home</div>
-    </div>
+      <Layout />
+    </>
   );
 }
 
