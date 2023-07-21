b6cafff659469de2689c8a8eabc78d9ea10b6297
chanyeol123 <chanyeol123@gmail.com>
2023-07-22 01:28:21 +0900

    style: add NewsFeedLayout

---
+++ src/pages/newsfeed/NewsFeedLayout.tsx
@@ -0,0 +1,13 @@
+import tw from 'twin.macro';
+
+function NewsFeedLayout() {
+    return (
+        <StyledNewsFeedLayout/>
+    );
+} 
+
+export default NewsFeedLayout;
+
+const StyledNewsFeedLayout = tw.div`
+items-center justify-center flex-row
+`
