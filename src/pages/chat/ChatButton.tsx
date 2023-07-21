fa09401518a371e29171df44ad9590c0bc238b43
Jae Yeon Hwang <>
2023-07-21 18:24:07 +0900

    style : add ChatMessageBox, ChatInputBox Layout

--- src/pages/chat/ChatButton.tsx
+++ src/pages/chat/ChatButton.tsx
@@ -6,10 +6,6 @@ export function ResumeWriteButton() {
     return <StyledWriteButton>이력서 작성하기</StyledWriteButton>;
 }
 
-export function ChatSendButton() {
-    return <StyleChatSendButton>전송</StyleChatSendButton>;
-}
-
 const StyledWriteButton = tw.button`
 w-[193px] 
 h-[58px] 
@@ -18,16 +14,5 @@ space-x-2.5
 px-[24px] 
 py-[16px] 
 bg-[#0177FD] 
-text-white
-`;
-
-const StyleChatSendButton = tw.button`
-w-[80px]
-h-[45px]
-rounded-[4px]
-space-x-2.5
-px-[24px]
-py-[12px]
-bg-[#0177FD]
 text-white
 `;
