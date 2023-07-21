b27cff64111b87b2500e2d1ec9f92de15953e8f8
Jae Yeon Hwang <>
2023-07-21 16:49:48 +0900

    style : add ChatListBox Layout

--- src/pages/chat/ChatListBox.tsx
+++ src/pages/chat/ChatListBox.tsx
@@ -1,14 +1,14 @@
-import tw from "twin.macro";
+import tw from 'twin.macro';
 
-function ChatListBox () {
+function ChatListBox() {
     return (
         <StyledChatListBox>
             <StyledProfileImg src="/images/profile.png" alt="Profile" />
-            <>
+            <StyledChatContent>
                 <StyledProfileName>홍길동</StyledProfileName>
-                <StyledChatPriview>안녕하세요. 이력서 보고...</StyledChatPriview>
-            </>
-            <StyledChatPriviewDate>12월 23일</StyledChatPriviewDate>
+                <StyledChatPreview>안녕하세요. 이력서 보고...</StyledChatPreview>
+            </StyledChatContent>
+            <StyledChatPreviewDate>12월 23일</StyledChatPreviewDate>
         </StyledChatListBox>
     );
 }
@@ -16,18 +16,25 @@ function ChatListBox () {
 export default ChatListBox;
 
 const StyledChatListBox = tw.div`
-w-[396px] h-[109px] space-y-[10px] px-[15px] py-6 border-b border-gray-300
+flex w-[396px] h-[109px] px-[15px] py-6 border-b border-gray-300
 `
 
 const StyledProfileImg = tw.img`
-w-[60px] h-[60px] rounded-full
+w-[60px] h-[60px] rounded-full mr-4
+`
+
+const StyledChatContent = tw.div`
+flex flex-col flex-grow
 `
 
 const StyledProfileName = tw.div`
+font-bold text-lg mb-2
 `
 
-const StyledChatPriview = tw.div`
+const StyledChatPreview = tw.div`
+text-sm text-gray-600
 `
 
-const StyledChatPriviewDate = tw.div`
+const StyledChatPreviewDate = tw.div`
+self-start ml-4 text-sm text-gray-400
 `
