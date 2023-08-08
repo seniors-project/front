03ef8d62ce63b0b52d2e2ea94cd672cdcfa98d65
ggujin <yang97816@gmail.com>
2023-08-08 22:31:32 +0900

    style: login page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,15 +1,51 @@
+import tw from 'twin.macro';
+import Link from 'next/link';
+
 import { kakaoLogin } from '@/utils/kakaoLogin';
 
 import { Layout } from '@/components/Layout';
+import { Container } from '@/styles';
+
+const Logo = tw.div`
+  w-28 mt-10 mx-auto
+`;
+
+const LoginHeader = tw.div`
+  mt-10 font-semibold text-3xl
+`;
+
+const LoginDescription = tw.div`
+  mt-6
+`;
+
+const BtnWapper = tw.button`
+ mt-7 mx-auto
+`;
 
 export default function Login() {
   return (
     <Layout>
-      <div>
-        <button title="카카오 로그인" onClick={kakaoLogin}>
-          이미지 대체예정
-        </button>
-      </div>
+      <Container>
+        <Logo>
+          <Link href="/">
+            <img src="/images/logo.png" />
+          </Link>
+        </Logo>
+        <div tw="max-w-lg mx-auto">
+          <LoginHeader>
+            <h1>회원가입</h1>
+          </LoginHeader>
+          <LoginDescription>
+            <p>
+              아이디, 비밀번호, 휴대폰번호 입력하기 귀찮으시죠? <br />
+              카카오로 1초만에 로그인 하세요!
+            </p>
+          </LoginDescription>
+          <BtnWapper title="카카오 로그인" onClick={kakaoLogin}>
+            <img src="/images/kakao.png" />
+          </BtnWapper>
+        </div>
+      </Container>
     </Layout>
   );
 }
