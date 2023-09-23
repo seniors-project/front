0a62783423906cbfaaceff6880f84c6da95e2efd
Jae Yeon Hwang <hjng0825@gmail.com>
2023-09-23 16:02:46 +0900

    feat : 채팅하기 버튼 스탕일링 완료 및 기능 수정

--- src/components/Button/ChatButton.tsx
+++ src/components/Button/ChatButton.tsx
@@ -5,28 +5,60 @@ import { useRouter } from 'next/router';
 interface ChatButtonProps {
   token: string;
   chatUserId: number;
+  backgroundColor?: string;
+  color?: string;
+  padding?: string;
+  fontSize?: string;
+  borderRadius?: string;
 }
 
-function ChatButton({ token, chatUserId }: ChatButtonProps) {
+interface ChatRoomResponse {
+  data: {
+    roomId: number;
+  };
+}
+function ChatButton({
+  token,
+  chatUserId,
+  backgroundColor = 'transparent',
+  color = 'black',
+  padding = '8px',
+  fontSize = '16px',
+  borderRadius = '4px',
+}: ChatButtonProps) {
   const router = useRouter();
   const handleChatClick = async () => {
     try {
       const response = await setCreateChatRoom(token, chatUserId);
+      const responseData: ChatRoomResponse = response.data;
 
-      console.log('Chat room created:', response);
-
-      router.push(`/chat/${chatUserId}`);
+      if (responseData.data && responseData.data.roomId) {
+        router.push(`/chat/${responseData.data.roomId}`);
+      } else {
+        console.error('Chat room creation response is missing data.');
+      }
     } catch (error) {
       console.error('Failed to create chat room:', error);
     }
   };
 
   return (
-    <StyledChatButton onClick={handleChatClick}>채팅하기</StyledChatButton>
+    <StyledChatButton
+      style={{
+        backgroundColor,
+        color,
+        padding,
+        fontSize,
+        borderRadius,
+      }}
+      onClick={handleChatClick}>
+      채팅하기
+    </StyledChatButton>
   );
 }
 
 export default ChatButton;
 
 const StyledChatButton = tw.div`
+cursor-pointer transition-all duration-300 ease-in-out hover:opacity-80
 `;
